5e39d4b46b45ead2625989756a6326456827c25f
ryanh-ops <ryanharris@oddlygood.shop>
2024-09-13 17:48:47 -0300

    create App.css, App.js, App.test.js and logo.svg

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+/* App.test.js */                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+const render = () => {
+  console.log("render");
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+};
+render();
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
