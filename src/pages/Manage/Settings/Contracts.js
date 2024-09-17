b004ec22fd4b79623bd9d4e0e3c8286a1c59de1a
ryanh-ops <ryanharris@oddlygood.shop>
2024-09-17 14:50:11 -0300

    update: contnstans & pages

---
+++ src/pages/Manage/Settings/Contracts.js
@@ -0,0 +1,3 @@
+export default function Contracts() {
+  return <>Contracts' Actions, here you can interact with the app's contracts soon.</>;
+}
---
+++ src/pages/Manage/Settings/Main/index.js
@@ -0,0 +1,16 @@
+import * as s from '../../../../styles/global';
+import IPFS from './IPFS';
+import Networks from './Networks';
+import Contracts from './Contracts';
+
+export default function Main() {
+  return (
+    <>
+      <IPFS />
+      <s.SpacerSmall />
+      <Networks />
+      <s.SpacerSmall />
+      <Contracts />
+    </>
+  );
+}
